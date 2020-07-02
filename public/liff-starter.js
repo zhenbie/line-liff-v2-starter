3d15542a0f1332f105ef13c7a8285f300fd559c0
zhenbaolei <zhenbaolei@jd.com>
2020-07-02 14:32:00 +0800

    int

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = true;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1654410354-LWE6PM3Y";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
