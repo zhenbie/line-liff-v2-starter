79abc7ced2448a46535e4b86434a702f3ccda88d
zhenbaolei <zhenbaolei@jd.com>
2020-07-02 17:55:32 +0800

    压

--- public/liff-starter.js
+++ public/liff-starter.js
@@ -55,6 +55,7 @@ function initializeLiff(myLiffId) {
             initializeApp();
         })
         .catch((err) => {
+			console.log('initerror',err)
             document.getElementById("liffAppContent").classList.add('hidden');
             document.getElementById("liffInitErrorMessage").classList.remove('hidden');
         });
